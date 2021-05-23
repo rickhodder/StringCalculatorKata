a98d6db42d724bf2dba21fb4085aa9fb9f7efb98
rickhodder <rickhodder@msn.com>
2021-05-23 11:38:59 -0400

    Zero returns zero

--- StringCalculatorTests.cs
+++ StringCalculatorTests.cs
@@ -6,6 +6,14 @@ namespace StringCalculatorKata
     public class StringCalculatorTests
     {
         [Fact]
+        public void Add_ZeroReturnsZero()
+        {
+            var sut = new StringCalculator();
+            var value = sut.Add("0");
+            Assert.Equal(0,value);
+        }
+
+        [Fact]
         public void Add_EmptyStringReturnsZero()
         {
             //create String Calculator with add method that takes an integer
